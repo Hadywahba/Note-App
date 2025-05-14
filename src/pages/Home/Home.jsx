b345cf1ff127a615f35842cbd51c03220302b12c
Hadywahba <hadywahba19@gmail.com>
2025-05-14 23:28:36 +0300

    Final02

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -67,7 +67,7 @@ export default function Home() {
        
        {notes?.length!=0 ? 
         <div className="py-8">
-         <h1 className="dark:text-white text-3xl font-serif font-bold mb-10 ">
+         <h1 className="dark:text-white text-3xl pt-10 font-serif font-bold mb-10 ">
           Notes
         </h1>
        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4  gap-6">
